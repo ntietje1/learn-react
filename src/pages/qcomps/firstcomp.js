09df550eafc03797d559d1a35b4e2c6fc36904ff
Nick <tietje.n@northeastern.edu>
2024-02-09 16:58:10 -0500

    worked through interactive examples

--- src/pages/qcomps/firstcomp.js
+++ src/pages/qcomps/firstcomp.js
@@ -1,1 +1,10 @@
-function MyComp() {}
+export default function MyComp() {
+    return (<div>
+        <h1>My First Component</h1>
+        <o1>
+            <li> Components: UI Building Block</li>
+            <li> Defining a Component</li>
+            <li> Using a component</li>
+        </o1>
+    </div>)
+}
